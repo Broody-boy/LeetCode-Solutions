fb2257198819635c56089425340c3be0d404a89b
Arshdeep Singh <111040555+Broody-boy@users.noreply.github.com>
2024-01-15 17:45:12 +0530

    Time: 3 ms (74.79%) | Memory: 9.8 MB (55.30%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -3,13 +3,17 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         bool flag = false;
 
-        for(int i = 0; i < matrix.size(); i++){
-            if(binary_search(matrix[i].begin(), matrix[i].end(), target)){
-                flag = true;
-                break;
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        for(int i = 0; i < m; i++){
+
+            if(target >= matrix[i][0]  && target <= matrix[i][n-1]){
+                return binary_search(matrix[i].begin(), matrix[i].end(), target);
             }
+
         }
 
-        return flag;
+        return false;
     }
 };
