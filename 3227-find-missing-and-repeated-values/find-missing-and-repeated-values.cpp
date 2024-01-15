9f44c940dcccb95af6d3fd74de97ec8e0f453a79
Arshdeep Singh <111040555+Broody-boy@users.noreply.github.com>
2024-01-15 14:01:41 +0530

    Time: 20 ms (81.36%) | Memory: 22.1 MB (94.37%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -2,27 +2,29 @@ class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
         
-        vector<int> ans(2);
-        unordered_map<int, int> m;
-
         int n = grid.size();
+        long long n2 = (long long)n * (long long)n;
+
+
+        long long sum = 0;
+        long long squareSum = 0;
+
+        long long nSum = (n2) * (n2 + 1) / 2;
+        long long nSquareSum = n2 * (n2 + 1) * (2*n2 + 1)/ 6;
 
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
-                m[grid[i][j]]++;
+                sum += grid[i][j];
+                squareSum += (long long)grid[i][j] * (long long)grid[i][j];
             }
         }
 
-        for(int i = 1; i <= n * n; i++){
+        long long Diff = sum - nSum;
+        long long Sum = (squareSum - nSquareSum)/Diff;
 
-            (m[i] == 2)
-                ? ans[0] = i
-            : (m[i] == 0) 
-                ? ans[1] = i
-            : 0;
-        }
+        int x = (Sum + Diff) / 2;
+        int y = (Sum - Diff) / 2;
 
-        return ans;
-
+        return {x,y};
     }
 };
