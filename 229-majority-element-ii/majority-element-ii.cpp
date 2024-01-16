60fe9a17e7c467a503ccd4aed92df19266fa106c
Arshdeep Singh <111040555+Broody-boy@users.noreply.github.com>
2024-01-16 13:10:58 +0530

    Time: 15 ms (23.75%) | Memory: 16.4 MB (40.86%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -2,18 +2,36 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         
-        map<int, int> mp;
+        int el1 = INT_MIN, el2 = INT_MIN, cnt1= 0, cnt2 = 0;
         int targetCount = nums.size()/3 + 1;
         vector<int> ans;
 
-        for(int num : nums){
-
-            mp[num]++;
-
-            if(mp[num] == targetCount)
-                ans.push_back(num);
-
+        for(int i = 0; i < nums.size(); i++){   
+            if(cnt1 == 0 && nums[i] != el2){
+                el1 = nums[i];
+                cnt1 = 1;
+            } else if(cnt2 == 0 && nums[i] != el1){
+                el2 = nums[i];
+                cnt2 = 1;
+            }
+            else if(nums[i] == el1) cnt1++;
+            else if(nums[i] == el2) cnt2++;
+            else{
+                cnt1--;
+                cnt2--;
+            }
         }
+
+
+        int count1 = 0, count2 = 0;
+
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == el1) count1++;
+            if(nums[i] == el2) count2++;
+        }
+
+        if(count1 >= targetCount) ans.push_back(el1);
+        if(count2 >= targetCount) ans.push_back(el2);
 
         return ans;
     }
