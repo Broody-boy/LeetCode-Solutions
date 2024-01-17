37a1b7f9d866a57205c4236bcb218f4156dfad91
Arshdeep Singh <111040555+Broody-boy@users.noreply.github.com>
2024-01-17 13:45:12 +0530

    Time: 4 ms (95.71%) | Memory: 10.7 MB (64.50%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,19 +1,44 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
-        unordered_map<int, int> mpp;
         int n = nums.size();
+        int left = 0;
+        int right = n-1;
+        vector<int> arr(n);
+        vector<int> ans;
 
-        for (int i = 0; i < n; i++) {
-            int num = nums[i];
-            int moreNeeded = target - num;
-            if (mpp.find(moreNeeded) != mpp.end()) {
-                return {mpp[moreNeeded], i};
-            }
-            mpp[num] = i;
+        for(int i = 0; i < n; i++){
+            arr[i] = nums[i];
         }
-        return { -1, -1};
 
+        sort(nums.begin(), nums.end());
+
+        while(left < right){
+            if(nums[left] + nums[right] == target){
+
+                for(int i = 0; i < n; i++){
+                    if(arr[i] == nums[left]){
+                        ans.push_back(i);
+                        break;
+                    }
+                }
+
+                for(int i = n-1; i >= 0; i--){
+                    if(arr[i] == nums[right]){
+                        ans.push_back(i);
+                        break;
+                    }
+                }
+
+                return ans;
+
+            }
+            else if(nums[left] + nums[right] < target)
+                left++;
+            else if(nums[left] + nums[right] > target)
+                right--;
+        }
+
+        return {-1,-1};
     }
 };
