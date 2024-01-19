b9e549932ae9a76df9d26a3fac6dbe4e14efd468
Arshdeep Singh <111040555+Broody-boy@users.noreply.github.com>
2024-01-20 00:27:11 +0530

    Time: 40 ms (49.78%) | Memory: 17 MB (20.35%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -14,7 +14,7 @@ public:
         ListNode* l1 = headA;
         ListNode* l2 = headB;
 
-        while(l1!=NULL || l2!=NULL){
+        while(l1!=NULL || l2!=NULL){        //Finding lengths of both linked lists in one go
             if(l1!=NULL){
                 length1++;
                 l1 = l1->next;
@@ -29,15 +29,15 @@ public:
         l1 = (length1>length2) ? headB : headA; //l1 is smaller one
         l2 = (length1>length2) ? headA : headB; //l2 is longer one
 
-        for(int i = 0; i < diff; i++)
+        for(int i = 0; i < diff; i++)       //Shredding the diff part from longer array's start
             l2 = l2->next;
         
-        while(l1!=l2){
+        while(l1!=l2){                      //Iterate until match
             l1 = l1->next;
             l2 = l2->next;
         }
 
-        return l1;
+        return l1;                          //or l2
 
     }
 };
