1a0795b69f21db1b1dc47fd6cc483a313159134a
Arshdeep Singh <111040555+Broody-boy@users.noreply.github.com>
2024-01-20 00:36:16 +0530

    Time: 38 ms (61.50%) | Memory: 17.2 MB (20.05%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -9,35 +9,16 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        int length1 = 0, length2 = 0;
-
-        ListNode* l1 = headA;
-        ListNode* l2 = headB;
-
-        while(l1!=NULL || l2!=NULL){        //Finding lengths of both linked lists in one go
-            if(l1!=NULL){
-                length1++;
-                l1 = l1->next;
-            }
-            if(l2!=NULL){
-                length2++;
-                l2 = l2->next;
-            }
-        }
-
-        int diff = abs(length2-length1);
-        l1 = (length1>length2) ? headB : headA; //l1 is smaller one
-        l2 = (length1>length2) ? headA : headB; //l2 is longer one
-
-        for(int i = 0; i < diff; i++)       //Shredding the diff part from longer array's start
-            l2 = l2->next;
         
-        while(l1!=l2){                      //Iterate until match
-            l1 = l1->next;
-            l2 = l2->next;
+        ListNode *a = headA;
+        ListNode *b = headB;
+
+        while(a!=b){
+            a = (a==NULL) ? headB: a->next;
+            b = (b==NULL) ? headA : b->next;
         }
 
-        return l1;                          //or l2
+        return a; //or b
 
     }
 };
