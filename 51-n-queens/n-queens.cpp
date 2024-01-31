f480a34483b155fc60e324a9bc0ba522c7cc4195
Arshdeep Singh <111040555+Broody-boy@users.noreply.github.com>
2024-01-31 06:06:51 -0800

    Time: 5 ms (63.66%) | Memory: 9.4 MB (25.78%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
 
-    void helper(int col, vector<string> &board, vector<vector<string>> &ans, vector<int> &leftRow, vector<int> &upperDiagonal, vector<int> &lowerDiagonal){
-        
+
+    void helper(int col, vector<string> &board, vector<vector<string>> &ans, vector<int> &lr, vector<int> &ld, vector<int> &ud){
         int n = board.size();
 
         if(col == n){
             ans.push_back(board);
-            return;
         }
 
         for(int row = 0; row < n; row++){
-            if(leftRow[row] == 0 && lowerDiagonal[row + col] == 0 && upperDiagonal[n-1 + col - row] == 0){
-                leftRow[row] = 1;
-                lowerDiagonal[row + col] = 1;
-                upperDiagonal[n-1 + col - row] = 1;
+            if(lr[row] == 0 && ld[row + col] == 0 && ud[n-1 + row-col] == 0){
+
+                lr[row] = 1;
+                ld[row + col] = 1; 
+                ud[n-1 + row-col] = 1;
 
                 board[row][col] = 'Q';
-                helper(col + 1, board, ans, leftRow, upperDiagonal, lowerDiagonal);
+                helper(col + 1, board, ans, lr, ld, ud);
                 board[row][col] = '.';
 
-                leftRow[row] = 0;
-                lowerDiagonal[row + col] = 0;
-                upperDiagonal[n-1 + col - row] = 0;
+                lr[row] = 0;
+                ld[row + col] = 0; 
+                ud[n-1 + row-col] = 0;
             }
         }
+
     }
 
     vector<vector<string>> solveNQueens(int n) {
@@ -32,17 +33,16 @@ public:
         vector<vector<string>> ans;
         vector<string> board;
 
+        vector<int> lr(n, 0);
+        vector<int> ld(2*n-1, 0);
+        vector<int> ud(2*n-1, 0);
+
         string s;
         for(int i = 0; i < n; i++) s.push_back('.'); // or direct: string s(n, '.');
 
         for(int i = 0; i < n; i++) board.push_back(s);
 
-
-        vector<int> leftRow(n, 0);
-        vector<int> upperDiagonal(2*n-1, 0);
-        vector<int> lowerDiagonal(2*n-1, 0);
-
-        helper(0, board, ans, leftRow, upperDiagonal, lowerDiagonal);
+        helper(0, board, ans, lr, ld, ud);
 
         return ans;
     }
