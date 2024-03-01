8caaeac9fa8d6a9b8b1a811c036d614cd70fdf61
Arshdeep Singh <111040555+Broody-boy@users.noreply.github.com>
2024-03-02 01:11:03 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (56.72%) - LeetSync

---
+++ 733-flood-fill/flood-fill.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+    bool canFlood(vector<vector<int>>& image, int i, int j, int startColor){
+        int m = image.size();
+        int n = image[0].size();
+        if(i<0 || j<0 || i>m-1 || j>n-1) return false;
+
+        return image[i][j]==startColor;
+    }
+
+    void helper(vector<vector<int>>& image, int i, int j, int startColor, int color){
+        if(canFlood(image, i+1, j, startColor)){
+            image[i+1][j] = color;
+            helper(image, i+1, j, startColor, color);
+        }
+        if(canFlood(image, i, j+1, startColor)){
+            image[i][j+1] = color;
+            helper(image, i, j+1, startColor, color);
+        }
+
+        if(canFlood(image, i-1, j, startColor)){
+            image[i-1][j] = color;
+            helper(image, i-1, j, startColor, color);
+        }
+
+        if(canFlood(image, i, j-1, startColor)){
+            image[i][j-1] = color;
+            helper(image, i, j-1, startColor, color);
+        }
+
+    }
+
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        if(image[sr][sc] == color) return image;
+        helper(image, sr, sc, image[sr][sc], color);
+        image[sr][sc] = color;
+        return image;
+    }
+};
