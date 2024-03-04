c3ee824e8eecaae34428dbc762477e59bd477de2
Arshdeep Singh <111040555+Broody-boy@users.noreply.github.com>
2024-03-05 00:31:29 +0530

    Time: 15 ms (75.93%) | Memory: 17.6 MB (38.89%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+
+        //convert to adjacency list:
+        vector<vector<int>> adj(numCourses);
+        for(auto pair : prerequisites){
+            adj[pair[0]].push_back(pair[1]);
+        }
+
+        vector<int> inDegree(numCourses, 0);
+        for(int node = 0; node < numCourses; node++){
+            for(int recipient : adj[node])
+                inDegree[recipient]++;
+        }
+
+        queue<int> q;
+        for(int node = 0; node < numCourses; node++){
+            if(inDegree[node] == 0) q.push(node);
+        }
+
+        vector<int> ans;
+        while(!q.empty()){
+            int front = q.front();
+            q.pop();
+            ans.push_back(front);
+
+            for(int neighbor : adj[front]){
+                inDegree[neighbor]--;
+                if(inDegree[neighbor] == 0) q.push(neighbor);
+            }
+        }
+
+        return !(ans.size() != numCourses); //cycle present => false. cycle not present => true
+    }
+};
