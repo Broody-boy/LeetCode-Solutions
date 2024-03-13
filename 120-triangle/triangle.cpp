1f08841de6e1bac6ba6e78975027f8657857e303
Arshdeep Singh <111040555+Broody-boy@users.noreply.github.com>
2024-03-13 11:16:32 +0530

    Time: 2 ms (89.43%) | Memory: 11.3 MB (45.74%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
 
-    int helper(int row, int col, int n, vector<vector<int>>& triangle, vector<vector<int>>& dp){
-        if(row == n-1) return dp[row][col] = triangle[row][col];
+    void helper(int row, int col, int n, vector<vector<int>>& triangle, vector<vector<int>>& dp){
+        
+        for(int col = 0; col < n; col++)
+            dp[n-1][col] = triangle[n-1][col];
 
-        if(dp[row][col] != -1) return dp[row][col];
-
-        int d = triangle[row][col] + helper(row+1, col, n, triangle, dp);
-        int dr = triangle[row][col] + helper(row+1, col+1, n, triangle, dp);
-
-        return dp[row][col] = min(d, dr);
+        for(int row = n-2; row >= 0; row--){
+            for(int col = row; col >= 0; col--){
+                int d = triangle[row][col] + dp[row+1][col];
+                int dr = triangle[row][col] + dp[row+1][col+1];
+                dp[row][col] = min(d, dr);
+            }
+        }
     }
 
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
 
-        vector<vector<int>>dp (n+1, vector<int>(n+1,-1));
-        return helper(0, 0, n, triangle, dp);
+        vector<vector<int>>dp (n+1, vector<int>(n+1, 0));
+        helper(0, 0, n, triangle, dp);
+        return dp[0][0];
     }
 };
