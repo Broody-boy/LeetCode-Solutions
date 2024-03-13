9e1cef431437c0d7b5fc8204c9e7df9262c65897
Arshdeep Singh <111040555+Broody-boy@users.noreply.github.com>
2024-03-13 12:23:58 +0530

    Time: 2 ms (89.43%) | Memory: 11 MB (68.25%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
 
-    void helper(int n, vector<vector<int>>& triangle, vector<vector<int>>& dp){
-        
+    void helper(int n, vector<vector<int>>& triangle, vector<int>& previous, vector<int>& current){
+
         for(int col = 0; col < n; col++)
-            dp[n-1][col] = triangle[n-1][col];
+            previous[col] = triangle[n-1][col];
 
         for(int row = n-2; row >= 0; row--){
             for(int col = row; col >= 0; col--){
-                int d = triangle[row][col] + dp[row+1][col];
-                int dr = triangle[row][col] + dp[row+1][col+1];
-                dp[row][col] = min(d, dr);
+                int d = triangle[row][col] + previous[col];
+                int dr = triangle[row][col] + previous[col+1];
+                current[col] = min(d, dr);
             }
+            previous = current;
         }
     }
 
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
 
-        vector<vector<int>>dp (n+1, vector<int>(n+1, 0));
-        helper(n, triangle, dp);
-        return dp[0][0];
+        vector<int>previous(n, 0);
+        vector<int>current(n, 0);
+        helper(n, triangle, previous, current);
+        return previous[0];
     }
 };
