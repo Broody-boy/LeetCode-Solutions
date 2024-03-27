77e110b5c813662700b6334cc12af88f2e2e725c
Arshdeep Singh <111040555+Broody-boy@users.noreply.github.com>
2024-03-27 15:37:50 +0530

    Time: 3 ms (84.90%) | Memory: 16.3 MB (23.27%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -51,12 +51,12 @@ public:
 
         if (fresh == 0) return 0; // No fresh oranges to rot
         
-        int ans = 0;
+        int ans = 0;              //Represents number of rounds taken
 
         while(!currRotten.empty()) {
             vector<pair<int, int>> nextRotten = returnNextRound(currRotten, fresh, grid);
             if(!nextRotten.empty()) ans++;
-            currRotten = nextRotten;
+            currRotten = nextRotten;  // For the next round, current round's nextRotten will become its currRotten.
         }
 
         return fresh == 0 ? ans : -1; // If there are still fresh oranges left, return -1
