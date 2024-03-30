ba643220d6ea84330a0acd32d23a0d4255402795
Arshdeep Singh <111040555+Broody-boy@users.noreply.github.com>
2024-03-30 21:51:43 +0530

    Time: 14 ms (31.47%) | Memory: 13.8 MB (61.40%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,37 +1,49 @@
 class Solution {
 public:
 
-    void dfs(int i, int j, vector<vector<char>>& board, vector<vector<int>>& vis){
+    void dfs(int i, int j, vector<vector<bool>>& vis, vector<vector<char>>& board){
+        
         int m = board.size();
         int n = board[0].size();
+        
+        if(i < 0 || i > m-1 || j < 0 || j > n-1 || vis[i][j]) return;
+        vis[i][j] = true;
 
-        if(i < 0 || j < 0 || i > m-1 || j > n-1 || vis[i][j] || board[i][j] != 'O') return;
-        vis[i][j] = 1;
+        if(board[i][j] == 'O'){
+            dfs(i+1, j, vis, board);
+            dfs(i, j+1, vis, board);
+            dfs(i-1, j, vis, board);
+            dfs(i, j-1, vis, board);
+        }
 
-        dfs(i+1, j, board, vis);
-        dfs(i-1, j, board, vis);
-        dfs(i, j+1, board, vis);
-        dfs(i, j-1, board, vis);
     }
 
     void solve(vector<vector<char>>& board) {
+        
         int m = board.size();
         int n = board[0].size();
+        
+        vector<vector<bool>> vis(m, vector<bool>(n, false));
 
-        vector<vector<int>> vis(m, vector<int>(n, 0));
-        for(int col = 0; col < n; col++){
-            if(!vis[0][col] && board[0][col] == 'O') dfs(0, col, board, vis);            
-            if(!vis[m-1][col] && board[m-1][col] == 'O') dfs(m-1, col, board, vis);
+        //left right:
+        for(int row = 0; row < m; row++){
+            //if(mat[row][0] == 'O' && !vis[row][0])
+            dfs(row, 0, vis, board);
+            //if(mat[row][n-1] == 'O' && !vis[row][n-1])
+            dfs(row, n-1, vis, board);
         }
 
-        for(int row = 0; row < m; row++){
-            if(!vis[row][0] && board[row][0] == 'O') dfs(row, 0, board, vis);
-            if(!vis[row][n-1] && board[row][n-1] == 'O') dfs(row, n-1, board, vis);
+        //top bottom:
+        for(int col = 0; col < n; col++){
+            //if(mat[0][col] == 'O' && !vis[0][col])
+            dfs(0, col, vis, board);
+            //if(mat[m-1][col] == 'O' && !vis[m-1][col])
+            dfs(m-1, col, vis, board);
         }
 
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(!vis[i][j] && board[i][j] == 'O') board[i][j] = 'X';
+                if(board[i][j] == 'O' && !vis[i][j]) board[i][j] = 'X';
             }
         }
 
