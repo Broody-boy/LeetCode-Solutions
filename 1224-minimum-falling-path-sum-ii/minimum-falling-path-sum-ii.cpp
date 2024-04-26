fc96c0095d1264eea3ae366cd996e4e5df3b6047
Arshdeep Singh <111040555+Broody-boy@users.noreply.github.com>
2024-04-26 20:38:36 +0530

    Time: 230 ms (49.32%) | Memory: 18.1 MB (57.11%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
 
-    int helper(int r, int prevCol, vector<vector<int>>& grid, vector<vector<int>>& dp){
-
-        if(r == grid.size()) return 0;
-
-        if(dp[r][prevCol] != -1) return dp[r][prevCol];
-        int mini = INT_MAX;
-        
-        for(int c = 0; c < grid[0].size(); c++){
-            if(c != prevCol)
-                mini = min(mini, grid[r][c] + helper(r+1, c, grid, dp));
-        }
-
-        return dp[r][prevCol] = mini;
-    }
-
-    int minFallingPathSum(vector<vector<int>>& grid) {
+    int helper(vector<vector<int>>& grid){
         int n = grid.size();
         vector<vector<int>> dp(n, vector<int>(n, -1));
 
-        int mini = INT_MAX;
+        for(int col = 0; col < n; col++) dp[n-1][col] = grid[n-1][col];
 
-        //For row 0:
-        for(int startCol = 0; startCol < n; startCol++)
-            mini = min(mini, grid[0][startCol] + helper(1, startCol, grid, dp));
+        for(int row = n-2; row >= 0; row--){
+            for(int col = 0; col < n; col++){
+                int mini = INT_MAX;
+                for(int colOfNextRow = 0; colOfNextRow < n; colOfNextRow++){
+                    if(colOfNextRow != col) mini = min(mini, grid[row][col] + dp[row+1][colOfNextRow]);
+                }
+                dp[row][col] = mini;
+            }
+        }
 
-        return mini;
+        int mini2 = INT_MAX;
+
+        for(int c = 0; c < n; c++){
+            mini2 = min(mini2, dp[0][c]);
+        }
+        return mini2;
+    }
+
+    int minFallingPathSum(vector<vector<int>>& grid) {
+        return helper(grid);
     }
 };
