4d87dd95eeed16554996e1aba48825155bba4070
Arshdeep Singh <111040555+Broody-boy@users.noreply.github.com>
2024-04-29 12:45:08 +0530

    Time: 7 ms (86.65%) | Memory: 15.4 MB (5.15%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -10,73 +10,33 @@
  */
 class Solution {
 public:
-
-    ListNode* reverseLL(ListNode* head){
-        if(head == NULL || head->next == NULL) return head;      
-
-        ListNode* newHead = reverseLL(head->next);
-        head->next->next = head;
-        head->next = NULL;
-        return newHead;
-    }
-
-    ListNode* findKthNode(ListNode* head, int k){
-        int i = 1;
-        ListNode *kthNode = head;
-
-        while(i!=k && kthNode!=NULL){
-            kthNode = kthNode->next;
-            i++;
-        }
-
-        return kthNode;
-    }
-
-    bool isCompleteGroup(ListNode* head, int k){
-
-        int length = 0;
-        ListNode*temp = head;
-
-        while(temp!=NULL){
-            temp = temp->next;
-            length++;
-        }
-
-        if (length < k) return false;
-        return true;
-    }
-
     ListNode* reverseKGroup(ListNode* head, int k) {
-
-        ListNode* nextNode = head;
-        ListNode* prevLast = NULL;
-        ListNode* temp = head;
-        ListNode* kthNode = NULL;
-
-        while(isCompleteGroup(temp, k)){
-
-            kthNode = findKthNode(temp, k);
-
-            nextNode = kthNode->next;       //Preserve the next block
-
-            kthNode->next = NULL;           //and make the block individual inked list:
-
-            reverseLL(temp);
-
-            temp->next = nextNode;
-
-            if(temp == head)
-                head = kthNode;             //For first group, update the head.
-            else
-                prevLast->next = kthNode;   //Else join this reversed block to previous chain
-
-            prevLast = temp;
-            temp = nextNode;
+        stack<ListNode*> stk;
+        ListNode* dummy = new ListNode(0);
+        ListNode* ptr = dummy;
+        ListNode* next_ptr = head;
+        
+        while (next_ptr != nullptr) {
+            ListNode* temp_ptr = next_ptr;
+            for (int i = 0; i < k && temp_ptr != nullptr; i++) {
+                stk.push(temp_ptr);
+                temp_ptr = temp_ptr->next;
+            }
+            
+            if (stk.size() == k) {
+                while (!stk.empty()) {
+                    ptr->next = stk.top();
+                    stk.pop();
+                    ptr = ptr->next;
+                }
+                ptr->next = temp_ptr;
+                next_ptr = temp_ptr;
+            } else {
+                ptr->next = next_ptr;
+                break;
+            }
         }
-
-        //for the remaining:
-        if(prevLast) prevLast->next = temp;
-
-        return head;
+        
+        return dummy->next;
     }
 };
